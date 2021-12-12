9ccb5d0ad127b93fb1153ae700a3c3099dde5b18
Atilla Altun <atilla.altun@ext.etiya.com>
2021-12-12 21:08:00 +0300

    ui created

---
+++ src/app/pages/note-list/note-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.scss'],
+})
+export class NoteListComponent implements OnInit {
+  cardTitle = 'Abc';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
