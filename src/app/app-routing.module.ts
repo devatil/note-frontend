761e2d1cd992d035f30f0ff971cab4a640d8a5e1
Atilla Altun <atilla.altun@ext.etiya.com>
2021-12-12 21:06:43 +0300

    ui design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: NoteListComponent,
+      },
+      {
+        path: ':id',
+        component: NoteDetailsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
